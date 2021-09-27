02dacdabab6668375efd39430538115b6ad3fcf3
Ranga Reddy <itsmerr17@gmail.com>
2021-09-27 12:19:54 +0530

    initial commit

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -13,6 +13,16 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    @IBAction func showAlert() {
+        let alert = UIAlertController(title: "Hello, World", message: "This is my relearning app dude!", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+    }
 
 
 }
